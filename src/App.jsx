ce46c9547c7b1ce2d368e44e7954b27dab3fe710
I-DUDE-I <thejusmathew8888@gmail.com>
2024-08-20 22:28:58 +0530

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -103,69 +103,115 @@ function App() {
   const[disDate,setDate] = useState(date)
   const[disMonth,setMonth] = useState(month)
   const[disYear,setYear] = useState(year)
-  const[input,setInput] = useState("")
-  const [weather, setWeather] = useState(null);
 
-  // weather data 
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+
+  // weather data
+
+  const demo = {
+    "weather": [
+      {
+        "id": 800,
+        "main": "Clear",
+        "description": "overcast clouds",
+        "icon": "10n"
+      }
+    ],
+    "main": {
+      "temp": 20,
+      "feels_like": 16.39,
+      "humidity": 69
+    },
+    "wind": {
+      "speed": 7.2
+    },
+    "sys": {
+      "country": "US"
+    },
+    "name": "New York"
+  }
+
   
 
+
+  const[input,setInput] = useState("")
+  const [weatherData, setWeatherData] = useState(demo);
+
+  
+  const [background, setBackground] = useState('./src/Images/clearSky.jpg');
+
+  useEffect(() => {
+    if (weatherData.weather[0].id === 800) {
+      setBackground('./src/Images/clearSky.jpg');
+    }
+     else if (weatherData.weather[0].id < 900 && weatherData.weather[0].id >800) {
+      setBackground('./src/Images/fullCloud.jpg');
+    }
+     else if (weatherData.weather[0].id < 600 && weatherData.weather[0].id >499) {
+      setBackground('./src/Images/rain.avif');
+    }
+     else if (weatherData.weather[0].id < 400 && weatherData.weather[0].id > 299) {
+      setBackground('./src/Images/shower.jpg');
+    }
+     else if (weatherData.weather[0].id < 700 && weatherData.weather[0].id > 599) {
+      setBackground('./src/Images/snow.jpg');
+    }
+     else if (weatherData.weather[0].id < 800 && weatherData.weather[0].id > 699) {
+      setBackground('./src/Images/mist.jpg');
+    }
+     else if (weatherData.weather[0].id < 300 && weatherData.weather[0].id > 199) {
+      setBackground('./src/Images/thunder.webp');
+    }
+  }, [weatherData]);
+
   const fetchWeatherData = async () => {
-    setLoading(true);
-    setError(null);
     try {
       const response = await fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=kochi&appid=b9b35dd7c967c2a9827580e8c51cbe91'
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=b9b35dd7c967c2a9827580e8c51cbe91&units=metric`
       );
       if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
+        throw error
       }
       const data = await response.json();
       setWeatherData(data);
-      console.log(data);
-      console.log(weatherData);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
+    } catch {
+      alert("City Name not found")
+      setInput("")
     }
   };
-  
 
-  useEffect(() => {
-    if (weatherData) {
-      console.log(3,weatherData); // Logs the updated weatherData
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      fetchWeatherData();
     }
-  }, [weatherData]);
+  };
   
 
   return (
     <>
       <div className="main">
-        <div className="bg1" style={{backgroundImage: "url(" + "/src/Images/clearSky.jpg" + ")"}}>
+        <div className="bg1" style={{backgroundImage: `url(${background})`}}>
           <div className="bg2">
           </div>
         </div>
         <div className="content">
-          <input type="text" onChange={(e)=>setInput(e.target.value)} placeholder='Enter City Name eg "New York"'/>
+          <input type="text" value={input} onKeyDown={handleKeyDown} onChange={(e)=>setInput(e.target.value)} placeholder='Enter City Name eg "New York"'/>
           <button onClick={fetchWeatherData}><i className="fa-solid fa-magnifying-glass"></i></button>
-          <h1 className='place'>New York</h1>
+          <h1 className='place'>{weatherData.name}</h1>
           <div className="temp">
-            <img src="https://openweathermap.org/img/wn/10n@2x.png" alt="" />
-            <span>20 &deg;C</span>
+            {weatherData.weather[0].main}<img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="" />
+            <span>{weatherData.main.temp} &deg;C</span>
           </div>
           <div className="details">
               <div className='time'>{disHour}:{disMin}<span className='AmPm'>{disAmPm}</span></div>
               <div className='date'>{disDay} {disDate} {disMonth} {disYear}</div>
               <div className="additional">
-                <span className='add'><i className="fa-solid fa-location-dot"></i>Country: (US)</span>
-                <span className='add'><i className="fa-solid fa-wind"></i>Wind: 1.65km/h</span>
+                <span className='add'><i className="fa-solid fa-location-dot"></i>Country: ({weatherData.sys.country})</span>
+                <span className='add'><i className="fa-solid fa-wind"></i>Wind: {weatherData.wind.speed}m/s</span>
               </div>
               <div className="additional">
-              <span className='add'><i className="fa-solid fa-droplet"></i>Humidity: 88%</span>
-              <span className='add'><i className="fa-solid fa-temperature-three-quarters"></i>Feels Like : 25°C</span>
+              <span className='add'><i className="fa-solid fa-droplet"></i>Humidity: {weatherData.main.humidity}%</span>
+              <span className='add'><i className="fa-solid fa-temperature-three-quarters"></i>Feels Like : {weatherData.main.feels_like}°C</span>
               </div>
           </div>
         </div>
